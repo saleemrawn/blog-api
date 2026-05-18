bd6c0fe6dd1693a094935518e56dcb2d1e218c3e
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-05-18 13:43:36 +0100

    feat: add middleware to parse json and urlencoded bodies from req

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.listen(process.env.PORT, (err) => {
   if (err) {
     throw err;
