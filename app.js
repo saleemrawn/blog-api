a79c87c2fb6e8ebfa250778d99c5ba05c6214679
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-05-22 14:56:49 +0100

    feat: mount auth and users API routes

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import "dotenv/config";
 import express from "express";
+import routes from "./routes/routes.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/api/auth", routes.auth);
+app.use("/api/users", routes.user);
 
 app.listen(process.env.PORT, (err) => {
   if (err) {
