cb677da68dd33f80d2a48620ca66a781f341c6b8
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-05-22 14:46:32 +0100

    feat: add POST route for login

---
+++ routes/auth.route.js
@@ -0,0 +1,10 @@
+import passport from "passport";
+import userRepository from "../repositories/user.repository.js";
+import authController from "../controllers/auth.controller.js";
+import { Router } from "express";
+
+const router = Router();
+
+router.post("/login", authController.login);
+
+export default router;
