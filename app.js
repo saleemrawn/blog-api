913d101f211c14388225af8e2bd5f64988c0624f
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-05-27 11:43:30 +0100

    feat: add cookie-parser middleware for JWT authentication
    - add cookie-parser to parse Cookie header and populate req.cookies with JWT token

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ import "dotenv/config";
 import "./config/passport.js";
 import express from "express";
 import routes from "./routes/routes.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use("/api/auth", routes.auth);
 app.use("/api/users", routes.user);
 
