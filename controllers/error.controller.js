f3126bf10fba192da1d1895dfb9248edef541f4e
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-06-07 13:14:21 +0100

    refactor: add err.meta?.model as primary field for Prisma errors
    - apply prettier formatting for res.status

--- controllers/error.controller.js
+++ controllers/error.controller.js
@@ -7,12 +7,12 @@ const getErrorStatusCode = (err) => {
 
 const getErrorMessage = (err) => {
   if (err.code === "P2025") {
-    const field = err.meta?.modelName ?? "field";
+    const field = err.meta?.model ?? err.meta?.modelName ?? "Record";
     return `${field} not found`;
   }
 
   if (err.code === "P2002") {
-    const field = err.meta?.modelName ?? "field";
+    const field = err.meta?.model ?? err.meta?.modelName ?? "field";
     return `A record for that ${field} already exists`;
   }
 
@@ -23,9 +23,7 @@ const errorController = (err, req, res, next) => {
   const statusCode = getErrorStatusCode(err);
   const errorMessage = getErrorMessage(err);
 
-  res
-    .status(statusCode)
-    .json({ success: false, code: err.code, message: errorMessage });
+  res.status(statusCode).json({ success: false, code: err.code, message: errorMessage });
 };
 
 export default errorController;
